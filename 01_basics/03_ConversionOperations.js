9f468a2eda0d7bccc7c2b372816f4490f589b5bb
ItzSherlock <gauravpsonawane07@gmail.com>
2025-01-12 12:32:49 +0530

    conversion opperations

---
+++ 01_basics/03_ConversionOperations.js
@@ -0,0 +1,31 @@
+let score = true
+
+// const {score} = req.body
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber));
+
+// console.log(valueInNumber);
+
+// "33" => 33
+// 33abc => NaN
+// true => 1 ; false=> 0
+// "" = > false
+// "abc" => true
+
+// let isLoggedIn = 1
+
+// let booleanInLoggedIN  = Boolean(isLoggedIn)
+// console.log(booleanInLoggedIN);
+
+let someNumber = 33
+let stringNumber  = String(someNumber)
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
+
+ 
